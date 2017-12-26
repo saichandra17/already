ec51a4a0ecaecd7b1a7af10f3c82e37f19feae6f
mac <mac@apple>
2017-12-26 14:59:00 -0500

    xxx

---
+++ src/main/java/com/websystique/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,16 @@
+package com.websystique.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.websystique.springmvc")
+public class HelloWorldConfiguration {
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
